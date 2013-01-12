a7a9fa7f3d9fb8ae8b02637cff2f84977f963c37
PoonKang <PoonKang@PoonKang.com>
2013-01-12 12:49:48 -0500

    fix remaining assembler errors

--- libm/Android.mk
+++ libm/Android.mk
@@ -204,7 +204,9 @@ ifeq ($(TARGET_ARCH),mips)
 	src/s_scalbln.c \
 	src/s_scalbn.c \
 	src/s_scalbnf.c \
-	src/e_sqrtf.c
+	src/e_sqrtf.c \
+	src/s_sin.c \
+	src/s_cos.c
 
   libm_common_includes = $(LOCAL_PATH)/mips
   # Need to build *rint* functions
